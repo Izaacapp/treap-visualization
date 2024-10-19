80a48af07624c1e49dc03b7fb5a86fcb83fac4f8
Izaacapp <izaacap@gmail.com>
2024-10-18 20:16:02 -0400

    .

--- static/transform-logic.js
+++ static/transform-logic.js
@@ -2,6 +2,7 @@ import {GeometryCircle, GeometryLine, GeometrySpace, GeometryText} from "./geome
 
 const RADIUS = 100;
 
+
 function get_margin(w) {
     return w / 10;
 }
@@ -14,30 +15,33 @@ function _tree_to_space(tree) {
     let [left, left_node] = _tree_to_space(tree.left);
     let [right, right_node] = _tree_to_space(tree.right);
 
-    let vertical_margin = RADIUS * 3; // Increase margin vertically to prioritize downward growth
-    let center_margin = RADIUS * 2; // Reduce horizontal margin to avoid excessive widening
+    let top_margin = RADIUS * 2;
+    if (left_node && right_node) {
+        top_margin += get_margin(left.w - left_node.x + 2 * RADIUS + right_node.x);
+    }
+    let center_margin = RADIUS * 3;
 
     let space = new GeometrySpace(
-        Math.max(left.w, right.w) + center_margin, // Width is the max of left and right subtrees plus some margin
-        left.h + vertical_margin + right.h // Height grows by adding both subtrees' heights and margin
+        left.w + center_margin + right.w,
+        top_margin + Math.max(left.h, right.h),
     );
 
-    space.project((space.w - left.w) / 2, 0, left); // Center left subtree horizontally
-    space.project((space.w - right.w) / 2, left.h + vertical_margin, right); // Position right subtree below left
+    space.project(0, top_margin, left);
+    space.project(left.w + center_margin, top_margin, right);
 
-    let vertex = new GeometryCircle(space.w / 2, RADIUS, RADIUS); // Place root node in the center
+    let vertex = new GeometryCircle(left.w + RADIUS, RADIUS, RADIUS);
 
     space.insert(vertex);
 
-    space.insert(new GeometryText(space.w / 2, RADIUS, tree.value, RADIUS * 2 / Math.max(2, tree.value.toString().length)));
-    space.insert(new GeometryText(space.w / 2 + 1.2 * RADIUS, RADIUS, tree.power, 50, "left"));
+    space.insert(new GeometryText(left.w + RADIUS, RADIUS, tree.value, RADIUS * 2 / Math.max(2, tree.value.toString().length)));
+    space.insert(new GeometryText(left.w + 2.2 * RADIUS, RADIUS, tree.power, 50, "left"));
 
     if (left_node != null) {
-        space.insert(new GeometryLine(vertex.x, vertex.y, left_node.x, left_node.y, RADIUS));
+        space.insert(new GeometryLine(left.w + RADIUS, RADIUS, left_node.x, left_node.y, RADIUS));
     }
 
     if (right_node != null) {
-        space.insert(new GeometryLine(vertex.x, vertex.y, right_node.x, right_node.y, RADIUS));
+        space.insert(new GeometryLine(left.w + RADIUS, RADIUS, right_node.x, right_node.y, RADIUS));
     }
 
     return [space, vertex];
